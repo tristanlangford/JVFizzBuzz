8d8ccd90b054f8c7a4436f795830612165a779b8
Tristan Langford <tristanlangford@hotmail.com>
2020-06-08 15:45:14 +0100

    first commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,13 @@
+describe("fizzbuzz", function () {
+  var fizzBuzz;
+
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz()
+  });
+
+  it('expect 1 to return 1', function () {
+    expect(fizzBuzz.number(1)).toEqual(1);
+  }
+
+}
